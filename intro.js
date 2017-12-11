5d5b5980111d1d88bfd65df1a49916e02323a9c7
Kyle Lin <klin12@stuy.edu>
2017-12-11 13:52:36 -0500

    Finished 16b

--- intro.js
+++ intro.js
@@ -1,12 +1,13 @@
 var b = document.getElementById("b");
 var heading = document.getElementById("h");
 var list = document.getElementById("thelist");
-var entries = document.getElementsByTagName("li");
+var entries = document.getElementsByClassName("items");
 
 var add = function(e) {
   var temp = document.createElement("li");
   var counter = entries.length;
   temp.innerHTML =  "item " + counter;
+  temp.setAttribute("class", "items");
   temp.addEventListener('mouseover', changeHead);
   temp.addEventListener('mouseout', changeHeadBack);
   temp.addEventListener('click', delListElement);
@@ -32,3 +33,44 @@ for (var i = 0; i < entries.length; i++) {
 }
 
 b.addEventListener('click', add);
+
+// Fib List
+
+var f = document.getElementById("f");
+var flist = document.getElementById("fiblist");
+var fentries = document.getElementsByClassName("fibclass");
+
+function fib(n) {
+  var a = 1;
+  var b = 0;
+  var temp;
+
+  while (n >= 0) {
+    temp = a;
+    a = a + b;
+    b = temp;
+    n--;
+  }
+
+  return b;
+};
+
+var fadd = function(e) {
+  var temp = document.createElement("li");
+  var counter;
+  var fel = fentries.length;
+  console.log(fel);
+  if (fel >= 0) {
+    counter = fel;
+  } else {
+    counter = 0;
+  }
+
+  temp.innerHTML = "item " + fib(counter);
+  temp.setAttribute("class", "fibclass");
+  temp.addEventListener('mouseover', changeHead);
+  temp.addEventListener('mouseout', changeHeadBack);
+  flist.appendChild(temp);
+};
+
+f.addEventListener('click', fadd);
